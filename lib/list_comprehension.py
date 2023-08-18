ce4b8fe4826062805e62663a0a56b8743ec1c961
MADERU-CM <cliv.moyia@student.moringaschool.com>
2023-08-18 16:06:17 +0300

    Assignment Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    even_elements = [x for x in sequence if x % 2 == 0]
+    return even_elements
 
-def make_exclamation(sentence_list):
-    pass
+# Example usage
+result = return_evens([0, 1, 3, 5, 7, 8, 9])
+print(result)  # Output: [0, 8]
+
+
+def make_exclamation(sentences):
+    exclamated_sentences = [sentence + '!' for sentence in sentences]
+    return exclamated_sentences
+
+# Example usage
+result = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(result)
+# Output: ["Hello!", "I'm doing great!", "Python is fun!"]
